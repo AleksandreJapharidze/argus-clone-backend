44aaf8b56891fcfd9449be620ec802a8413d92da
AleksandreJapharidze <alexanderjapharidze@gmail.com>
2025-12-13 08:27:01 +0400

    Optimized another query about fetching course results. Very cool!

--- src/main/java/com/example/argusclone/repositories/StudentCourseResultRepository.java
+++ src/main/java/com/example/argusclone/repositories/StudentCourseResultRepository.java
@@ -1,12 +1,14 @@
 package com.example.argusclone.repositories;
 
 import com.example.argusclone.entities.StudentCourseResult;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface StudentCourseResultRepository extends JpaRepository<StudentCourseResult, Integer> {
+    @EntityGraph(attributePaths = {"student", "course"})
     List<StudentCourseResult> findByStudentId(Integer studentId);
     Optional<StudentCourseResult> findByStudentIdAndCourseId(Integer studentId, Integer courseId);
     void deleteByCourseId(Integer courseId);
