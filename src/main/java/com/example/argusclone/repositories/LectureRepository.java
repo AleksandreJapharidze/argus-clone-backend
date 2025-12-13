36bdabafbf5df6886ffc5f9b385657b3e6374bc7
AleksandreJapharidze <alexanderjapharidze@gmail.com>
2025-12-13 22:06:04 +0400

    Forgot to clean LectureRepo. Sry~

--- src/main/java/com/example/argusclone/repositories/LectureRepository.java
+++ src/main/java/com/example/argusclone/repositories/LectureRepository.java
@@ -3,12 +3,5 @@ package com.example.argusclone.repositories;
 import com.example.argusclone.entities.Lecture;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.Optional;
-
 public interface LectureRepository extends JpaRepository<Lecture, Integer> {
-    Optional<Lecture> findByLectureDateAndLectureStartTimeAndLectureEndTimeAndRoomNumber(
-            LocalDate lectureDate, LocalTime lectureStartTime, LocalTime lectureEndTime, String roomNumber
-    );
 }
